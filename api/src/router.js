8c024c2f6243629d726c62c0e2013db6c2e3fa07
Fernando Lemos <fecalemos@gmail.com>
2021-04-27 09:53:42 -0300

    Completed module

--- api/src/router.js
+++ api/src/router.js
@@ -27,9 +27,13 @@ const upload = multer({
 });
 
 router.post('/upload', upload.single('photo'), (request, response) => {
-    if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError});
+    if (request.fileValidationError) return response.status(400).json({
+        error: request.fileValidationError
+    });
     
-    return response.status(201).json({success: true});
+    return response.status(201).json({
+        success: true
+    });
 })
 
 module.exports = router;
